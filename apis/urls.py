0a902c1ce13dc84b3c6a1fed302216b3bb43366d
Alex <dnjscksdn98@gmail.com>
2020-01-08 22:10:00 +0900

    Add logout API path

--- apis/urls.py
+++ apis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apis.views import UserCreateView, UserLoginView
+from apis.views import UserCreateView, UserLoginView, UserLogoutView
 
 urlpatterns = [
-    path('v1/users/create/', UserCreateView.as_view(), name='apis_v1_user_create'),
-    path('v1/users/login/', UserLoginView.as_view(), name='apis_v1_user_login'),
+    path('v1/user/create/', UserCreateView.as_view(), name='apis_v1_user_create'),
+    path('v1/user/login/', UserLoginView.as_view(), name='apis_v1_user_login'),
+    path('v1/user/logout/', UserLogoutView.as_view(), name='apis_v1_user_logout'),
 ]
